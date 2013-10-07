3f28d21930258cef6c08796ab4cede242505214e
Tom Rhodes <hello@madeinebor.com>
2013-10-07 11:47:20 +0100

    Fixed Marble (Again)

--- themes/marble.php
+++ themes/marble.php
@@ -26,6 +26,12 @@ function seabird_blockquote( $atts, $content = null ) {
 }
 add_shortcode('blockquote', 'seabird_blockquote');
 
+//BREAK [break]
+function seabird_break( $atts, $content = null ) {
+   return '<div style="width: 100%; clear: both; height: 40px;"></div><hr /><div style="width: 100%; clear: both; height: 40px;"></div>';
+}
+add_shortcode('break', 'seabird_break');
+
 //CLEAR [clear]
 function seabird_clear( $atts, $content = null ) {
    return '<div class="divide20"></div>';
@@ -61,3 +67,55 @@ function seabird_social_icon( $atts, $content = null ) {
     return '<li><a href="'.esc_url($link).'" '.$target.'><i class="icon-s-'.$icon.'"></i></a></li>';
 }
 add_shortcode('social_icon', 'seabird_social_icon');
+
+/*-----------------------------------------------------------------------------------*/
+/*	SHORTCODE GENERATOR
+/*-----------------------------------------------------------------------------------*/
+
+//SHORTCODE DROP DOWN
+add_action('media_buttons','add_sc_select',11);
+function add_sc_select(){
+    global $shortcode_tags; $shortcodes_list='';
+    /** Any Shortcodes that should be excluded from this list should be added below */
+    $exclude = array("wp_caption", "embed", "gallery", "caption", "audio", "video", "template", "rev_slider");
+    echo '<select style="float:right;margin-right:15px;position:relative;top:5px;height:20px;" id="sc_select">
+    <option>Insert Shortcode...</option>';
+    foreach ($shortcode_tags as $key => $val){
+            if(!in_array($key,$exclude)){
+            	if($key=='blockquote'){
+            		$output = "[blockquote author='John Doe']Content[/blockquote]";
+            	} elseif($key=='button'){
+            		$output = "[button link='google.com' size='large' color='blue' target='blank']Link Text[/button]";
+            	} elseif($key=='clear'){
+            		$output = "[clear]";
+            	} elseif($key=='break'){
+            		$output = "[break]";
+            	} elseif($key=='highlight'){
+            		$output = "[highlight]Content[/highlight]";
+            	} elseif($key=='dropcap'){
+            		$output = "[dropcap]Content[/dropcap]";
+            	} elseif($key=='social_group'){
+            		$output = "[social_group][social_icon link='google.com' target='blank' icon='facebook'][/social_group]";
+            	} elseif($key=='social_icon'){
+            		$output = "[social_icon link='google.com' target='blank' icon='facebook']";
+            	} else {
+            	 $option = "[$key][/$key]";
+            	}
+            $shortcodes_list .= '<option value="'.$output.'">'.$key.'</option>';
+            }
+        }
+     echo $shortcodes_list;
+     echo '</select>';
+}
+
+add_action('admin_head', 'button_js');
+function button_js() {
+    echo '<script type="text/javascript">
+    jQuery(document).ready(function(){
+        jQuery("#sc_select").change(function() {
+            send_to_editor(jQuery("#sc_select :selected").val());
+                return false;
+            });
+        });
+    </script>';
+}
