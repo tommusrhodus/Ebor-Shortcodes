530f38abbf3cef96cc9ecbfec53dddc00158b3ef
Tom Rhodes <hello@madeinebor.com>
2013-11-07 18:53:26 +0000

    fixed major issue!

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ Author URI: http://www.madeinebor.com
 $theme_name = wp_get_theme();
 
 if( $theme_name == 'ShadowBox' || $theme_name->parent() == 'ShadowBox' ) {
-		require_once( plugin_dir_path( __FILE__ ) .'/themes/shadowbox.php' );
+		require_once( plugin_dir_path( __FILE__ ) .'themes/shadowbox.php' );
 } elseif( $theme_name == 'Marble' || $theme_name->parent() == 'Marble' ) {
-		require_once( plugin_dir_path( __FILE__ ) .'/themes/marble.php' );
+		require_once( plugin_dir_path( __FILE__ ) .'themes/marble.php' );
 } elseif( $theme_name == 'Wiretree' || $theme_name->parent() == 'Wiretree' ){
-		require_once( plugin_dir_path( __FILE__ ) .'/themes/wiretree.php' );
+		require_once( plugin_dir_path( __FILE__ ) .'themes/wiretree.php' );
 } elseif( $theme_name == 'Other' || $theme_name->parent() == 'Other' ){
-		require_once( plugin_dir_path( __FILE__ ) .'/themes/other.php' );
+		require_once( plugin_dir_path( __FILE__ ) .'themes/other.php' );
 } else {
-		require_once( plugin_dir_path( __FILE__ ) .'/themes/searbid.php' );
+		require_once( plugin_dir_path( __FILE__ ) .'themes/searbid.php' );
 }
