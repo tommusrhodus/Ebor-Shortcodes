5428f0efa25d0bdf745b445a7ee6aa5c73dcc3bd
Tom Rhodes <hello@madeinebor.com>
2013-11-21 17:22:19 +0000

    Added New

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if( $theme_name == 'ShadowBox' || $theme_name->parent() == 'ShadowBox' ) {
 		require_once( plugin_dir_path( __FILE__ ) .'themes/marble.php' );
 } elseif( $theme_name == 'Wiretree' || $theme_name->parent() == 'Wiretree' ){
 		require_once( plugin_dir_path( __FILE__ ) .'themes/wiretree.php' );
-} elseif( $theme_name == 'Other' || $theme_name->parent() == 'Other' ){
+} elseif( $theme_name == 'Other' || $theme_name->parent() == 'Other' || $theme_name == 'New' || $theme_name->parent() == 'New' ){
 		require_once( plugin_dir_path( __FILE__ ) .'themes/other.php' );
 } else {
 		require_once( plugin_dir_path( __FILE__ ) .'themes/seabird.php' );
